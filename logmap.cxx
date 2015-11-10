98535089722110e4dac5b03ad99baae3c2b0bc42
Camilla Willim <cwillim@pc02.pool.tp1.uni-duesseldorf.de>
2015-11-10 19:36:15 +0100

    guten Abend.#

--- logmap.cxx
+++ logmap.cxx
@@ -1,23 +1,28 @@
 #include <iostream>
-
+#include <fstream>
 using namespace std;
 
-int main(){
+int main(void){
 	double x0 = 0.5;
-	double x;
+	double xs;
 	int Nskip = 100; //Number of iterations to skip
 	int Nend  = 200; //Number of total iterations
-
+	double x[Nend-Nskip];
+	ofstream out("output.dat");
+	
 	for(double r=0; r <= 4; r += 0.001){
-	   x=x0;
-	   for(int i=1; i <= Nskip; i++)
-		   x = r*x*(1-x);
-	   for(int i=Nskip+1; i <= Nend; i++){
-	   		   x = r*x*(1-x);
-	   		   cout << r << "\t" << x << endl;
+	  xs=x0;
+	   for(int i=1; i <= Nskip+1; i++)
+		   xs = r*xs*(1-xs);
+	   x[0]=xs;
+	   for(int i=Nskip+2; i <= Nend; i++){
+	   		   x[i-(Nskip+1)] = r*x[i-(Nskip+2)]*(1-x[i-(Nskip+2)]);
    	   }
+   	   for(int i=0; i<(Nend-Nskip); i++){
+	     out << r << "\t" << x[i] << endl;
+	   }
 	}
 
-
+	out.close();
 	return 0;
 }
